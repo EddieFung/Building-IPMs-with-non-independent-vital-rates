9c5d593b49d9832ff11b6e10fd07e537e7513bbb
Eddie Fung <37061371+EddieFung@users.noreply.github.com>
2021-03-09 09:21:35 +0000

    Update run2.R

--- run2.R
+++ run2.R
@@ -1,4 +1,16 @@
 #sheep lambda approximation
+replication = 500
+core = 5 #number of cores for used, it took us around 2 hours to finish with 5 cores
+source("fn3.R")
+store1 = matrix(0, nrow = replication, ncol = 8) #store lambda
+store2 = matrix(0, nrow = replication, ncol = 8) #store population mean mass
+store3 = matrix(0, nrow = replication, ncol = 8) #store population variance mass
+NAO  = read.table("NAOc", header = FALSE)
+NAO1 = read.table("NAO" , header = FALSE)
+NAO1 = cbind(NAO1[2:12,1], (rowSums(NAO1[2:12,2:4]) + NAO1[1:11,13]) / 4) #NAO data in 1986-1996
+NAO3 = cbind(NAO[22:51,1], (rowSums(NAO[22:51,2:4]) + NAO[21:50,13]) / 4) #NAO data in 1990-2019
+NAO5 = cbind(NAO[ 2:51,1], (rowSums(NAO[ 2:51,2:4]) + NAO[ 1:50,13]) / 4) #NAO data in 1970-2019
+
 ISSample  = as.matrix(read.table(paste(getwd(), "/sample/ISSample" , sep=""), header = TRUE))
 indSample = as.matrix(read.table(paste(getwd(), "/sample/indSample", sep=""), header = TRUE))
 repSample = as.matrix(read.table(paste(getwd(), "/sample/repSample", sep=""), header = TRUE))
@@ -8,20 +20,9 @@ iitSample = as.matrix(read.table(paste(getwd(), "/sample/iitSample", sep=""), he
 mitSample = as.matrix(read.table(paste(getwd(), "/sample/mitSample", sep=""), header = TRUE))
 iiiSample = as.matrix(read.table(paste(getwd(), "/sample/iiiSample", sep=""), header = TRUE))
 miiSample = as.matrix(read.table(paste(getwd(), "/sample/miiSample", sep=""), header = TRUE))
-
-gamma = 1
-replication = 500
-core = 6 #number of cores for used, it took us around 2 hours to finish with 6 cores
-source("fn3.R")
+id0 = sample(1:dim(miiSample)[1], replication, replace = TRUE)
+miiSample = miiSample[id1,] #we do sampling on model VIII here to reduce the memory
 n1 = dim(ISSample)[1]
-store1 = matrix(0, nrow = replication, ncol = 8) #store lambda
-store2 = matrix(0, nrow = replication, ncol = 8) #store population mean mass
-store3 = matrix(0, nrow = replication, ncol = 8) #store population variance mass
-NAO  = read.table("NAOc", header = FALSE)
-NAO1 = read.table("NAO" , header = FALSE)
-NAO1 = cbind(NAO1[2:12,1], (rowSums(NAO1[2:12,2:4]) + NAO1[1:11,13]) / 4) #NAO data in 1986-1996
-NAO3 = cbind(NAO[22:51,1], (rowSums(NAO[22:51,2:4]) + NAO[21:50,13]) / 4) #NAO data in 1990-2019
-NAO5 = cbind(NAO[ 2:51,1], (rowSums(NAO[ 2:51,2:4]) + NAO[ 1:50,13]) / 4) #NAO data in 1970-2019
 
 time = Sys.time()
 id0 = sample(1:n1, replication, replace = TRUE)
@@ -35,8 +36,6 @@ store1[,1] = temp[,1]; store2[,1] = temp[,2]; store3[,1] = temp[,3] #independent
 
 id0 = sample(1:n1, replication, replace = TRUE)
 id1 = sample(1:n1, replication, replace = TRUE)
-iitSample[,6] = iitSample[,6] * gamma 
-iitSample[,7] = iitSample[,7] * gamma
 temp = mclapply(1:replication, function(x) 
   SIM_mit(1000,5000,50,1,4,
           ISSample[id0[x],3:4],ISSample[id0[x],c(1,2,5)],
@@ -48,8 +47,6 @@ store1[,2] = temp[,1]; store2[,2] = temp[,2]; store3[,2] = temp[,3] #uncorrelate
 id0 = sample(1:n1, replication, replace = TRUE)
 id1 = sample(1:dim(iiiSample)[1], replication, replace = TRUE)
 iiiSample = iiiSample[id1,]
-iiiSample[,6] = iiiSample[,6] * gamma 
-iiiSample[,7] = iiiSample[,7] * gamma 
 temp = mclapply(1:replication, function(x) 
   SIM_mii(50,30,50,1,4,
           ISSample[id0[x],3:4],ISSample[id0[x],c(1,2,5)],
@@ -60,7 +57,6 @@ store1[,3] = temp[,1]; store2[,3] = temp[,2]; store3[,3] = temp[,3]  #uncorrelat
 
 id0 = sample(1:n1, replication, replace = TRUE)
 id1 = sample(1:n1, replication, replace = TRUE)
-repSample[,3] = repSample[,3] * gamma 
 temp = mclapply(1:replication, function(x) 
   lam_rep(50,1,4,ISSample[id0[x],3:4],ISSample[id0[x],c(1,2,5)],
           repSample[id1[x],c(1,2,6)],repSample[id1[x],4:5],repSample[id1[x],3]),
@@ -81,8 +77,6 @@ store1[,5] = temp[,1]; store2[,5] = temp[,2]; store3[,5] = temp[,3] #copula mode
 
 id0 = sample(1:n1, replication, replace = TRUE)
 id1 = sample(1:n1, replication, replace = TRUE)
-driSample[,3] = driSample[,3] * gamma 
-driSample[,6] = driSample[,6] * gamma 
 temp = mclapply(1:replication, function(x) 
   SIM_dri1(1000,5000,50,1,4,
            ISSample[id0[x],3:4],ISSample[id0[x],c(1,2,5)],
@@ -100,8 +94,6 @@ store1[,6] = temp[,1]; store2[,6] = temp[,2]; store3[,6] = temp[,3] #shared driv
 
 id0 = sample(1:n1, replication, replace = TRUE)
 id1 = sample(1:n1, replication, replace = TRUE)
-mitSample[,7] = mitSample[,7] * gamma 
-mitSample[,8] = mitSample[,8] * gamma
 temp = mclapply(1:replication, function(x) 
   SIM_mit(1000,5000,50,1,4,
           ISSample[id0[x],3:4],ISSample[id0[x],c(1,2,5)],
@@ -111,10 +103,6 @@ temp = matrix(unlist(temp), ncol = 3, byrow = TRUE)
 store1[,7] = temp[,1]; store2[,7] = temp[,2]; store3[,7] = temp[,3]  #correlated random year effect models
 
 id0 = sample(1:n1, replication, replace = TRUE)
-id1 = sample(1:dim(miiSample)[1], replication, replace = TRUE)
-miiSample = miiSample[id1,]
-miiSample[,7] = miiSample[,7] * gamma 
-miiSample[,8] = miiSample[,8] * gamma
 temp = mclapply(1:replication, function(x) 
   SIM_mii(50,30,50,1,4,
           ISSample[id0[x],3:4],ISSample[id0[x],c(1,2,5)],
