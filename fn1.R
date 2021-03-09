f98fc1c5899d9d5990c7944ba29af3734a406657
Eddie Fung <37061371+EddieFung@users.noreply.github.com>
2021-03-09 09:11:04 +0000

    Update fn1.R

--- fn1.R
+++ fn1.R
@@ -15,6 +15,34 @@ indString <- nimbleCode({ #independent model
   param2[2]~dnorm(0, sd = 100)
   sigma_g~dunif(0,5)
 })
+repString <- nimbleCode({ #reproduction conditional model
+  pred1[1:N1,1] <- X1[1:N1,1:3] %*% param1[1:3]
+  pred2[1:N2,1] <- X2[1:N2,1:2] %*% param2[1:2]
+  for (i in 1:N1) Y1[i] ~ dnorm(pred1[i,1], sd = sigma_g) #pred1[i]
+  for (i in 1:N2) Y2[i] ~ dbinom(1/(1+exp(-pred2[i,1])),1)
+  
+  param1[1]~dnorm(0, sd = 100)
+  param1[2]~dnorm(0, sd = 100)
+  param1[3]~dnorm(0, sd = 100)
+  param2[1]~dnorm(0, sd = 100)
+  param2[2]~dnorm(0, sd = 100)
+  sigma_g~dunif(0,5)
+})
+copString <- nimbleCode({  #copula model
+  pred1[1:N1,1] <- X1[1:N1,1:2] %*% param1[1:2] 
+  pred2[1:N1,1] <- X1[1:N1,1:2] %*% param2[1:2]
+  pred3[1:N3,1] <- X3[1:N3,1:2] %*% param2[1:2] 
+  for (i in 1:N1) Y1[i] ~ dnorm(pred1[i,1], sd = sigma_g) 
+  for (i in 1:N1) Y2[i]~dbinom(iprobit((probit(1-expit(pred2[i,1]))-alpha/sigma_g*(Y1[i]-pred1[i,1]))/sqrt(1 - alpha**2)),1) 
+  for (i in 1:N3) Y3[i] ~ dbinom(1/(1+exp(-pred3[i,1])),1)
+  
+  param1[1]~dnorm(0, sd = 100)
+  param1[2]~dnorm(0, sd = 100)
+  param2[1]~dnorm(0, sd = 100)
+  param2[2]~dnorm(0, sd = 100)
+  sigma_g~dunif(0,5)
+  alpha~dunif(-1,1)
+})
 driString <- nimbleCode({ #shared drivers model
   pred1[1:N1,1] <- X1[1:N1,1:3] %*% param1[1:3]
   pred2[1:N2,1] <- X2[1:N2,1:3] %*% param2[1:3]
@@ -29,43 +57,6 @@ driString <- nimbleCode({ #shared drivers model
   param2[3]~dnorm(0, sd = 100)
   sigma_g~dunif(0,5)
 })
-repString <- nimbleCode({ #reproduction conditional model
-  pred1[1:N1,1] <- X1[1:N1,1:3] %*% param1[1:3]
-  pred2[1:N2,1] <- X2[1:N2,1:2] %*% param2[1:2]
-  for (i in 1:N1) Y1[i] ~ dnorm(pred1[i,1], sd = sigma_g) #pred1[i]
-  for (i in 1:N2) Y2[i] ~ dbinom(1/(1+exp(-pred2[i,1])),1)
-  
-  param1[1]~dnorm(0, sd = 100)
-  param1[2]~dnorm(0, sd = 100)
-  param1[3]~dnorm(0, sd = 100)
-  param2[1]~dnorm(0, sd = 100)
-  param2[2]~dnorm(0, sd = 100)
-  sigma_g~dunif(0,5)
-})
-
-copString <- nimbleCode({  #copula model
-  pred1[1:N1,1] <- X1[1:N1,1:2] %*% param1[1:2] + Z1[1:N1,1:noI ] %*% u[1:noI,1]
-  pred2[1:N2,1] <- X2[1:N2,1:2] %*% param2[1:2] + Z2[1:N2,1:noI ] %*% u[1:noI,2]
-  pred3[1:N3,1] <- X3[1:N3,1:2] %*% param2[1:2] + Z3[1:N3,1:noI2] %*% v[1:noI2 ]
-  for (i in 1:N1) Y1[i] ~ dnorm(pred1[i,1], sd = sigma_g) 
-  for (i in 1:N2) Y2[i] ~ dbinom(1/(1+exp(-pred2[i,1])),1)
-  for (i in 1:N3) Y3[i] ~ dbinom(1/(1+exp(-pred3[i,1])),1)
-  
-  param1[1]~dnorm(0, sd = 100)
-  param1[2]~dnorm(0, sd = 100)
-  param2[1]~dnorm(0, sd = 100)
-  param2[2]~dnorm(0, sd = 100)
-  sigma_g~dunif(0,5)
-  W[1:2,1:2] ~ dwish(Omega[1:2,1:2],3)
-  for (i in 1:noI ) u[i,1:2] ~ dmnorm(mu[1:2],W[1:2,1:2]) #W is a precision matrix
-  for (i in 1:noI2) v[i]     ~ dnorm(0,sd = tau_r)
-  
-  W_inv[1:2,1:2] <- inverse(W[1:2,1:2])
-  tau_g <- sqrt(W_inv[1,1])
-  tau_r <- sqrt(W_inv[2,2])
-  rho <- W_inv[1,2] / tau_g / tau_r
-})
-
 iitString <- nimbleCode({ #uncorrelated random year effect model
   pred1[1:N1,1] <- X1[1:N1,1:2] %*% param1[1:2] + Z1[1:N1,1:noI] %*% u[1:noI,1]
   pred2[1:N2,1] <- X2[1:N2,1:2] %*% param2[1:2] + Z2[1:N2,1:noI] %*% u[1:noI,2]
